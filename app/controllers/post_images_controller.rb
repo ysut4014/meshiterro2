f4bdf104e96aa7c28febfa5630cc4135d02b0c9f
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2023-10-27 19:57:49 +0000

    20章【共通のレイアウトを部分テンプレートでまとめよう】

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,12 +16,14 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   def destroy
     post_image = PostImage.destroy(params[:id])
     post_image.destroy
     redirect_to post_images_path
   end
+  
    # 投稿データのストロングパラメータ
   private
 
