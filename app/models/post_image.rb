b0c29a50cb71a25f4ef3fa24af3dc0032e558924
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2023-10-28 13:04:05 +0000

    22章【ページネーションを学ぼう】

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,9 +1,11 @@
 class PostImage < ApplicationRecord
-      has_one_attached :image
-      belongs_to :user
-      has_many :post_comments, dependent: :destroy
-      has_many :favorites, dependent: :destroy
-      
+     has_one_attached :image
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
    def favorited_by?(user)
     favorites.exists?(user_id: user.id)
    end
